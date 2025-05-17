1412ab65ed2daa333ecb2b4333ef4a8a575b6262
Vidsa <vidsa2002@gmail.com>
2025-05-17 19:17:09 +0530

    Initial commit

--- 0075_Sort_Colors.py
+++ 0075_Sort_Colors.py
@@ -5,15 +5,16 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         l,r=0,len(nums)-1
-        i=0
-        while i<=r:
-            if nums[i]==0:
-                nums[i],nums[l]=nums[l],nums[i]
-                l+=1
+        i=0 
+        while i<=r: 
+            if nums[i]==0: 
+                nums[i],nums[l]=nums[l],nums[i] # swap
+                l+=1 # move left pointer
             elif nums[i]==2:
                 nums[i],nums[r]=nums[r],nums[i]
-                r-=1
-                i-=1
+                r-=1 # move right pointer
+                i-=1 # decrement i to check the swapped element once again because it might be 0 or 1 
+                # outer if condition there is a increment of i
             i+=1 
         return nums      
 
