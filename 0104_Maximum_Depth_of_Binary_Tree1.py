b95d4a4a415e4343dddcee09ce2dbdba2317ebb0
Vidsa <vidsa2002@gmail.com>
2025-05-27 19:11:59 +0530

    Initial commit

--- 0104_Maximum_Depth_of_Binary_Tree1.py
+++ 0104_Maximum_Depth_of_Binary_Tree1.py
@@ -1,4 +1,5 @@
-# using recursive Depth-first search (DFS) to find the maximum depth of a binary tree
+# using breadth-first search (BFS) to find the maximum depth of a binary tree
+from collections import deque
 from typing import Optional
 
 class TreeNode:
@@ -11,14 +12,30 @@ class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        level = 0
+        q = deque([root])
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
 
 if __name__ == "__main__":
+    # Example tree:
+    #       1
+    #      / \
+    #     2   3
+    #    / \
+    #   4   5
     root = TreeNode(1)
     root.left = TreeNode(2)
     root.right = TreeNode(3)
     root.left.left = TreeNode(4)
     root.left.right = TreeNode(5)
-    
+
     sol = Solution()
     print(sol.maxDepth(root))  # Output: 3
